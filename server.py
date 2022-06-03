b0e2e286dbf8039fa5ca7b404975d5659c0efed5
dyson1357 <dyson1357@naver.com>
2022-06-03 17:15:18 +0900

    Flask 생활 코딩 실습 완료

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
-from flask import Flask
-from flask import request, redirect
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
+
 nextId = 4
-#리스트, 딕셔너리를 활용하여 메모리를 사용하기 때문에 나중에 값 수정할 경우 날아감,,, 일반적으로 DB에서 다룸
 topics = [
     {'id': 1, 'title': 'html', 'body': 'html is ...'},
     {'id': 2, 'title': 'css', 'body': 'css is ...'},
     {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
 ]
 
-def template(contents, content):
+
+def template(contents, content, id=None):
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li><a href="/update/{id}/">update</a></li>
+            <li><form action="/delete/{id}/" method="POST"><input type="submit" value="delete"></form></li>
+        '''
     return f'''<!doctype html>
     <html>
         <body>
@@ -21,11 +27,13 @@ def template(contents, content):
             </ol>
             {content}
             <ul>
-            <li><a href="/create/">create</a></li>
+                <li><a href="/create/">create</a></li>
+                {contextUI}
             </ul>
         </body>
     </html>
     '''
+
 
 def getContents():
     liTags = ''
@@ -33,25 +41,26 @@ def getContents():
         liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
     return liTags
 
+
 @app.route('/')
 def index():
-
     return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
 
-@app.route('/read/<int:id>/')   #<> 이렇게 하면, 이 자리에 있는 값을 받아올 수 있음
+
+@app.route('/read/<int:id>/')
 def read(id):
-        title = ''
-        body = ''
-        for topic in topics:
-            if id == topic['id']:
-                title = topic['title']
-                body = topic['body']
-                break
-        return template(getContents(), f'<h2>{title}</h2>{body}')
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    return template(getContents(), f'<h2>{title}</h2>{body}', id)
+
 
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
-    print('request.method', request.method)
     if request.method == 'GET':
         content = '''
             <form action="/create/" method="POST">
@@ -69,8 +78,47 @@ def create():
         topics.append(newTopic)
         url = '/read/'+str(nextId)+'/'
         nextId = nextId + 1
-
         return redirect(url)
+
+
+@app.route('/update/<int:id>/', methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'GET':
+        title = ''
+        body = ''
+        for topic in topics:
+            if id == topic['id']:
+                title = topic['title']
+                body = topic['body']
+                break
+        content = f'''
+            <form action="/update/{id}/" method="POST">
+                <p><input type="text" name="title" placeholder="title" value="{title}"></p>
+                <p><textarea name="body" placeholder="body">{body}</textarea></p>
+                <p><input type="submit" value="update"></p>
+            </form>
+        '''
+        return template(getContents(), content)
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        for topic in topics:
+            if id == topic['id']:
+                topic['title'] = title
+                topic['body'] = body
+                break
+        url = '/read/'+str(id)+'/'
+        return redirect(url)
+
+
+@app.route('/delete/<int:id>/', methods=['POST'])
+def delete(id):
+    for topic in topics:
+        if id == topic['id']:
+            topics.remove(topic)
+            break
+    return redirect('/')
 
 
 app.run(debug=True)
