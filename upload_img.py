ce548621445bd86ae5a7397b47440d7c109a4dd6
dyson1357 <dyson1357@naver.com>
2022-06-07 14:08:16 +0900

    업로드한 파일 목록 출력

--- upload_img.py
+++ upload_img.py
@@ -1,46 +1,39 @@
-import imghdr
 import os
-from flask import Flask, render_template, request, redirect, url_for, abort, \
-    send_from_directory
+from flask import Flask, request, flash, redirect
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
-app.config['UPLOAD_PATH'] = './uploaded_img'
-
-def validate_image(stream):
-    header = stream.read(512)
-    stream.seek(0)
-    format = imghdr.what(None, header)
-    if not format:
-        return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
-
-@app.errorhandler(413)
-def too_large(e):
-    return "File is too large", 413
 
 @app.route('/')
-def index():
-    files = os.listdir(app.config['UPLOAD_PATH'])
-    return render_template('index.html', files=files)
+def upload_main():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>File Upload</title>
+    </head>
+    <body>
+        <form action="http://localhost:5000/file-upload" method="POST" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <input type="submit">
+        </form>
+    </body>
+    </html>"""
 
-@app.route('/', methods=['POST'])
+@app.route('/file-upload', methods=['GET', 'POST'])
 def upload_files():
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            return "Invalid image", 400
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    return '', 204
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('./uploaded_img/' + secure_filename(f.filename))
 
-@app.route('/uploads/<filename>')
-def upload(filename):
-    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+        return """
+             <script>
+                alert("File upload complete");
+                window.location = '/';
+             </script>
+        """
 
 if __name__ == '__main__':
     app.run(debug=True)
