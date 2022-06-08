db8c65ee4a04de80a14d2b85461ea9458c839499
dyson1357 <74144011+dyson1357@users.noreply.github.com>
2022-06-08 21:15:00 +0900

    주석 작업 중

--- upload_img.py
+++ upload_img.py
@@ -1,7 +1,8 @@
 import imghdr
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
+import convert
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
@@ -21,11 +22,14 @@ def validate_image(stream):
 def too_large(e):
     return "File is too large", 413
 
+#기본 주소
 @app.route('/')
 def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
+    #파일 업로드 동작(드롭다운 등)은 static의 js 파일에 정의되어 있음
     return render_template('index.html', files=files)
 
+#파일 업로드 동작
 @app.route('/', methods=['POST'])
 def upload_files():
     uploaded_file = request.files['file']
@@ -35,9 +39,12 @@ def upload_files():
         if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
                 file_ext != validate_image(uploaded_file.stream):
             return "Invalid image", 400
+
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], "before.png"))
+        convert.main()
     return '', 204
 
+#캐쉬 문제 해결 코드
 @app.after_request
 def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
