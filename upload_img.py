f97e8e1b76e671116b967fe18579571ab35d6723
dyson1357 <74144011+dyson1357@users.noreply.github.com>
2022-06-08 22:19:38 +0900

    Update upload_img.py

--- upload_img.py
+++ upload_img.py
@@ -6,7 +6,9 @@ import convert
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+#input 데이터의 확장자 조건
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+#이미지가 저장될 경로
 app.config['UPLOAD_PATH'] = 'static/img/'
 
 
@@ -18,15 +20,18 @@ def validate_image(stream):
         return None
     return '.' + (format if format != 'jpeg' else 'jpg')
 
+#오류 핸들러, 파일 크기가 너무 크면 413 에러를 뱉어냄
+#에러 핸들러로 리다이렉트 되지 않아 오류 핸들러 작성
 @app.errorhandler(413)
 def too_large(e):
-    return "File is too large", 413
+    return "파일이 너무 큽니다!", 413
 
 #기본 주소
 @app.route('/')
 def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
     #파일 업로드 동작(드롭다운 등)은 static의 js 파일에 정의되어 있음
+    #전체 사이트 껍데기 index.html에 있음
     return render_template('index.html', files=files)
 
 #파일 업로드 동작
@@ -36,10 +41,11 @@ def upload_files():
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
+        #파일 확장자가 jpg, png, gif가 아니면 400번 에러 뱉음
         if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
                 file_ext != validate_image(uploaded_file.stream):
             return "Invalid image", 400
-
+        #다른 에러 처리기에 걸리지 않으면 input 이미지를 before.png라는 이름과 확장자를 가진 파일로 저장
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], "before.png"))
         convert.main()
     return '', 204
