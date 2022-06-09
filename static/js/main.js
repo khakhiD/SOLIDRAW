5c1bd6e204cb2e89c08cb0bffbb8e6d39f4b53fe
khakhiD <bfwanso@naver.com>
2022-06-10 00:45:39 +0900

    수정중

--- static/js/main.js
+++ static/js/main.js
@@ -1,139 +1,207 @@
-/*=============== SUBMIT BUTTON ==========================*/ 
-function loadFile(input) {
-  let file = input.files[0];
-  let name = document.getElementById("fileName");
-  name.textContent = file.name;
-
-  let newImage = document.createElement("img");
-  newImage.setAttribute("class", "img");
-  newImage.src = URL.createObjectURL(file);
-  newImage.style.width = "300px";
-  newImage.style.height = "auto";
-  newImage.style.visibility = "hidden"; //버튼을 누르기 전까지는 이미지 숨기기
-  newImage.style.objectFit = "contain";
-
-  let container = document.getElementById("image-show");
-  container.appendChild(newImage);
+const spinner = document.getElementById('loading');
+function loadingSpin(){
+    //로딩 표시
+    showLoading();
+    //로딩 숨기기
+    setTimeout("hideLoading()", 1500);
+}
+function showLoading(){
+    document.getElementById('loading').style.display = 'block';
+    document.body.style.opacity = '.95';
+}
+function hideLoading(){
+    document.getElementById('loading').style.display = 'none';
+    document.body.style.opacity = '1';
 }
 
+
+/*=============== SUBMIT BUTTON ==========================*/
+function loadFile(input) {
+    let file = input.files[0];	//선택된 파일 가져오기
+    loadingSpin();
+    //미리 만들어 놓은 div에 text(파일 이름) 추가
+    let name = document.getElementById('fileName');
+    name.textContent = file.name;
+
+  	//새로운 이미지 div 추가
+    let newImage = document.createElement("img");
+    newImage.setAttribute("class", 'img');
+
+    //이미지 source 가져오기
+    newImage.src = URL.createObjectURL(file);
+
+    newImage.style.width = "70%";
+    newImage.style.height = "70%";
+    newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지를 숨긴다
+    newImage.style.objectFit = "contain";
+
+    //이미지를 image-show div에 추가
+    let container = document.getElementById('image-show');
+    container.appendChild(newImage);
+    newImage = document.getElementById('image-show').lastElementChild;
+    newImage.style.visibility ="visible";
+}
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
 let swiperPopular = new Swiper(".popular__container", {
-    spaceBetween: 32,
-    grabCursor: true,
-    centeredSlides: false,
-    slidesPerView: 'auto',
-    loop: false,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: "auto",
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 /*=============== VALUE ACCORDION ===============*/
-const accordionItems = document.querySelectorAll('.value__accordion-item')
+const accordionItems = document.querySelectorAll(".value__accordion-item");
 
 accordionItems.forEach((item) => {
-    const accordionHeader = item.querySelector('.value__accordion-header')
-    accordionHeader.addEventListener('click', ()=>{
-        const openItem = document.querySelector('.accordion-open')
+  const accordionHeader = item.querySelector(".value__accordion-header");
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
 
-        toggleItem(item);
+    toggleItem(item);
 
-        if(openItem && openItem!== item){
-            toggleItem(openItem)
-
-        }
-    })
-})
-const toggleItem = (item) => {
-    const accordionContent = item.querySelector('.value__accordion-content')
-
-    if(item.classList.contains('accordion-open')){
-        accordionContent.removeAttribute('style')
-        item.classList.remove('accordion-open')
-    }else{
-        accordionContent.style.height = accordionContent.scrollHeight + 'px'
-        item.classList.add('accordion-open')
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
     }
-}
+  });
+});
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
-        
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
-/*=============== SHOW SCROLL UP ===============*/ 
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    if(this.scrollY >= 350) {
-        scrollUp.classList.add('show-scroll')
-    } else scrollUp.classList.remove('show-scroll')
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 350) {
+    scrollUp.classList.add("show-scroll");
+  } else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
-/*=============== DARK LIGHT THEME ===============*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
 
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
 
-if(selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
-themeButton.addEventListener('click', () => {
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon)
-})
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon);
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    // reset: true
-})
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
 
-sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
-sr.reveal(`.home__description, .footer__info`, {delay: 500})
-sr.reveal(`.home__search`, {delay: 600})
-sr.reveal(`.home__value`, {delay: 700})
-sr.reveal(`.home__images`, {delay: 800, origin: 'bottom'})
-sr.reveal(`.logos__img`, {interval: 100})
-sr.reveal(`.value__images, .contact__content`, {origin: 'left'})
-sr.reveal(`.value__content, .contact__images`, {origin: 'right'})
-
-
+sr.reveal(
+  `.home__title, .popular__container, .subscribe__container, .footer__container`
+);
+sr.reveal(`.home__description, .footer__info`, { delay: 500 });
+sr.reveal(`.home__search`, { delay: 600 });
+sr.reveal(`.home__value`, { delay: 700 });
+sr.reveal(`.home__images`, { delay: 800, origin: "bottom" });
+sr.reveal(`.logos__img`, { interval: 100 });
+sr.reveal(`.value__images, .contact__content`, { origin: "left" });
+sr.reveal(`.value__content, .contact__images`, { origin: "right" });
 
 /*===================== NUMBER COUNT ANIMATION =======================*/
+
+/* ==========================IMAGE UPLOAD=============================*/
+
+
+// let file = document.getElementById('chooseFile');
+// file.onchange = function(e) {
+//     const currentBtn = document.getElementById("upload__button");
+//     const submitBtn = document.getElementById("submit__button");
+//     let files = e.target.files;
+//     if (files !== null) {
+//         console.log(submitBtn);
+//         if(currentBtn.style.display !== 'none'){
+//             currentBtn.style.display = 'none';
+//             submitBtn.style.display = 'flex';
+//         } else {
+//             currentBtn.style.display = 'flex';
+//             submitBtn.style.display = 'none';
+//         }
+//     }
+// }
+
+function page_update() {
+    location.reload(true);
+  }
+
+
+/* LOADING SPINNER */
+function showSpinner() {
+    document.getElementsByClassName('loading')[0].style.display='block';
+}
+function hideSpinner() {
+    document.getElementsByClassName('loading')[0].style.display='none';
+}
--- upload_img.py
+++ upload_img.py
@@ -9,7 +9,7 @@ app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 #input 데이터의 확장자 조건
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 #이미지가 저장될 경로
-app.config['UPLOAD_PATH'] = 'solidraw/static/img/'
+app.config['UPLOAD_PATH'] = 'solidraw/temp/'
 
 
 def validate_image(stream):
@@ -32,23 +32,28 @@ def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
     #파일 업로드 동작(드롭다운 등)은 static의 js 파일에 정의되어 있음
     #전체 사이트 껍데기 index.html에 있음
-    return render_template('index2.html', files=files)
+    return render_template('index.html', files=files)
 
 #파일 업로드 동작
 @app.route('/', methods=['POST'])
 def upload_files():
+    print('uploader start')
     uploaded_file = request.files['file']
+    print('file loaded')
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
         #파일 확장자가 jpg, png, gif가 아니면 400번 에러 뱉음
         if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
+            file_ext != validate_image(uploaded_file.stream):
             return "Invalid image", 400
         #다른 에러 처리기에 걸리지 않으면 input 이미지를 before.png라는 이름과 확장자를 가진 파일로 저장
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], "before.png"))
+        print('file saved')
         convert.main()
     return '', 204
+
+
 
 #캐쉬 문제 해결 코드
 @app.after_request
